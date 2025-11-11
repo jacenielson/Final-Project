e5be6537e0bfdbbbf099308c1f9f9691b9a75d96
jacenielson <jace.nielson@students.snow.edu>
2025-11-11 09:04:59 -0700

    Card Class Implemented

--- Junko.Logic/Card.cs
+++ Junko.Logic/Card.cs
@@ -1,5 +1,25 @@
 namespace Junko.Logic;
-public class Card
+public class Card : ICard
 {
+    private static int nextId = 0;
 
+    public int ID { get; }
+    public string Name { get; }
+    public CardColor Color { get; }
+    public CardType Type { get; }
+    public string Value { get; }
+
+    public Card(CardColor color, string value)
+    {
+        ID = nextId;
+        Color = color;
+        Value = value;
+        Type = CardType.Number;
+        Name = $"{color} {value}";
+    }
+    public void Play(GameRunner game, Player player)
+    {
+        game.Stack.Add(this);
+    }
+    public override string ToString() => Name;
 }
--- Junko.Logic/ICard.cs
+++ Junko.Logic/ICard.cs
@@ -1,8 +1,27 @@
 namespace Junko.Logic;
+
 public interface ICard
 {
     int ID { get; }
-    string Color { get; }
+    CardColor Color { get; }
+    CardType Type { get; }
     string Value { get; }
-    
+    void Play(GameRunner game, Player player);
+}
+public enum CardColor
+{
+    Red,
+    Blue,
+    Green,
+    Yellow,
+    Wild
+}
+public enum CardType
+{
+    Number,
+    Skip,
+    Reverse,
+    DrawTwo,
+    Wild,
+    WildDrawFour
 }
